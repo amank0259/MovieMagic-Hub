7440c4cb5ea278021cf9553188895883d831dbb4
Aman kumar <amank0259@gmail.com>
2024-04-09 11:43:05 +0530

    add card component to show the trending

---
+++ src/templates/Cards.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Cards({ data, title }) {
+    document.title = "Trending"
+    return (
+        <div className='flex flex-wrap w-full'>
+            {data.map((item, index) => (
+                <Link className='w-[25vh] mr-[3%] mb-[5%]' key={index}>
+                    <img className='shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[40vh] object-cover' src={`https://image.tmdb.org/t/p/original/${item.poster_path || item.backdrop_path}`} alt="" />
+                    <h1 className='text-2xl text-zinc-200 mt-5 font-semibold'>
+                        {item.name || item.title || item.original_name || item.original_title}
+                    </h1>
+
+                </Link>
+            ))}
+        </div>
+    )
+}
+
+export default Cards
