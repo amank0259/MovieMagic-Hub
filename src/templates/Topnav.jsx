5d2c92d04f5727063b0a7f95748faca927785827
Aman kumar <amank0259@gmail.com>
2024-04-10 17:16:04 +0530

    fix search result and inhance UI

--- src/templates/Topnav.jsx
+++ src/templates/Topnav.jsx
@@ -27,9 +27,9 @@ function Topnav() {
             <input onChange={(e) => setSearch(e.target.value)} value={search} className='relative w-[50%] mx-10 p-5 text-xl outline-none border-none bg-transparent focus:outline-1 outline-1 focus:outline-secondary outline-zinc-500 rounded-full text-zinc-200' type='text' placeholder='search anything' />
             {search.length > 0 && (<i onClick={() => setSearch("")} className="text-zinc-300 text-2xl ri-close-fill"></i>)}
 
-            {search.length > 0 && (<div className='absolute w-[55%] max-h-[50vh] overflow-auto top-[100%] bg-zinc-200 rounded'>
+            {search.length > 0 && (<div className='absolute z-50 w-[55%] max-h-[50vh] overflow-auto top-[100%] bg-zinc-200 rounded'>
                 {searchResult.map((item, index) => (
-                    <Link key={index} className='w-[100%] text-zinc-600 font-semibold p-10 flex justify-start items-center border-b-2 border-zinc-100 hover:text-black hover:bg-zinc-300 duration-300'>
+                    <Link key={index} className='w-[100%] text-zinc-600 font-semibold p-10 flex justify-start items-center border-b-2 border-zinc-100 hover:text-black hover:bg-secondary duration-300'>
                         <img className='w-[10vh] h-[10vh] object-cover rounded mr-5 shadow-lg' src={item.backdrop_path || item.profile_path ? `https://image.tmdb.org/t/p/original/${item.backdrop_path || item.profile_path}` : noimage} alt="" />
                         <span>{item.title || item.originaltitle || item.name}</span>
                     </Link>
