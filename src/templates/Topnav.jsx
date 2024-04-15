41d9f6115820f52d62b334cf91dd5ac0e167af0b
Aman kumar <amank0259@gmail.com>
2024-04-15 11:16:02 +0530

    make the clickable link to redirect the movie/tv_show page

--- src/templates/Topnav.jsx
+++ src/templates/Topnav.jsx
@@ -29,7 +29,7 @@ function Topnav() {
 
             {search.length > 0 && (<div className='absolute z-50 w-[55%] max-h-[50vh] overflow-auto top-[100%] bg-zinc-200 rounded'>
                 {searchResult.map((item, index) => (
-                    <Link key={index} className='w-[100%] text-zinc-600 font-semibold p-10 flex justify-start items-center border-b-2 border-zinc-100 hover:text-black hover:bg-secondary duration-300'>
+                    <Link to={`/${item.media_type}/details/${item.id}`} key={index} className='w-[100%] text-zinc-600 font-semibold p-10 flex justify-start items-center border-b-2 border-zinc-100 hover:text-black hover:bg-secondary duration-300'>
                         <img className='w-[10vh] h-[10vh] object-cover rounded mr-5 shadow-lg' src={item.backdrop_path || item.profile_path ? `https://image.tmdb.org/t/p/original/${item.backdrop_path || item.profile_path}` : noimage} alt="" />
                         <span>{item.title || item.originaltitle || item.name}</span>
                     </Link>
